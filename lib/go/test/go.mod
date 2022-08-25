58db4b5835fcf2c2af276fbb3c4be2101585d37e
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2022-08-25 14:27:39 -0700

    Move cadence to cadence folder

--- lib/go/test/go.mod
+++ lib/go/test/go.mod
@@ -3,8 +3,22 @@ module github.com/onflow/fcl-contracts/lib/go/test
 go 1.14
 
 require (
-	github.com/onflow/cadence v0.23.0
-	github.com/onflow/flow-emulator v0.29.0
-	github.com/onflow/flow-go-sdk v0.24.0
-	github.com/stretchr/testify v1.7.1-0.20210824115523-ab6dc3262822
+	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
+	github.com/census-instrumentation/opencensus-proto v0.3.0 // indirect
+	github.com/cncf/xds/go v0.0.0-20211130200136-a8f946100490 // indirect
+	github.com/codahale/hdrhistogram v0.9.0 // indirect
+	github.com/envoyproxy/protoc-gen-validate v0.6.2 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20190430165422-3e4dfb77656c // indirect
+	github.com/jrick/bitset v1.0.0 // indirect
+	github.com/libp2p/go-libp2p-autonat v0.6.0 // indirect
+	github.com/libp2p/go-yamux/v2 v2.3.0 // indirect
+	github.com/onflow/cadence v0.25.0
+	github.com/onflow/flow-emulator v0.34.1
+	github.com/onflow/flow-go v0.27.4 // indirect
+	github.com/onflow/flow-go-sdk v0.26.5
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	github.com/smartystreets/assertions v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.0
+	github.com/uber/jaeger-client-go v2.29.1+incompatible // indirect
+	github.com/uber/jaeger-lib v2.4.0+incompatible // indirect
 )
--- lib/go/test/templates.go
+++ lib/go/test/templates.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Contracts struct {
-	FCLCrypto flow.Address
+	FCLCrypto                    flow.Address
 	FlowInteractionTemplateAudit flow.Address
 }
 
@@ -15,17 +15,23 @@ var fclCryptoPlaceholder = regexp.MustCompile(`"[^"\s].*/FCLCrypto.cdc"`)
 var flowInteractionTemplatePlaceholder = regexp.MustCompile(`"[^"\s].*/FlowInteractionTemplateAudit.cdc"`)
 
 const (
-	fclCryptoPath                    = "../../../contracts/FCLCrypto.cdc"
-	scriptsRootPath                  = "../../../scripts"
-	flowInteractionTemplatePath			 = "../../../contracts/FlowInteractionTemplateAudit.cdc"
-	verifyUserSignaturesPath         = scriptsRootPath + "/FCLCrypto/verifyUserSignatures.cdc"
-	verifyAccountProofSignaturesPath = scriptsRootPath + "/FCLCrypto/verifyAccountProofSignatures.cdc"
+	fclCryptoPath                                 = "../../../cadence/contracts/FCLCrypto.cdc"
+	scriptsRootPath                               = "../../../cadence/scripts"
+	transactionsRootPath                          = "../../../cadence/transactions"
+	flowInteractionTemplatePath                   = "../../../cadence/contracts/FlowInteractionTemplateAudit.cdc"
+	flowCreateInteractionTemplateAuditManagerPath = transactionsRootPath + "/FlowInteractionTemplateAudit/create-audit-manager.transaction.cdc"
+	flowAddInteractionTemplateAuditPath           = transactionsRootPath + "/FlowInteractionTemplateAudit/add-audit.transaction.cdc"
+	flowRevokeInteractionTemplateAuditPath        = transactionsRootPath + "/FlowInteractionTemplateAudit/revoke-audit.transaction.cdc"
+	flowHasAuditedInteractionTemplateAuditPath    = scriptsRootPath + "/FlowInteractionTemplateAudit/get-has-audited-from-audit-manager.script.cdc"
+	verifyUserSignaturesPath                      = scriptsRootPath + "/FCLCrypto/verifyUserSignatures.cdc"
+	verifyAccountProofSignaturesPath              = scriptsRootPath + "/FCLCrypto/verifyAccountProofSignatures.cdc"
 )
 
 func replaceAddresses(codeBytes []byte, contracts Contracts) []byte {
 	code := string(codeBytes)
 
 	code = fclCryptoPlaceholder.ReplaceAllString(code, "0x"+contracts.FCLCrypto.String())
+	code = flowInteractionTemplatePlaceholder.ReplaceAllString(code, "0x"+contracts.FlowInteractionTemplateAudit.String())
 
 	return []byte(code)
 }
@@ -36,6 +42,34 @@ func fclCryptoContract() []byte {
 
 func flowInteractionTemplateContract() []byte {
 	return readFile(flowInteractionTemplatePath)
+}
+
+func flowCreateInteractionTemplateAuditManagerScript(contracts Contracts) []byte {
+	return replaceAddresses(
+		readFile(flowCreateInteractionTemplateAuditManagerPath),
+		contracts,
+	)
+}
+
+func flowAddInteractionTemplateAuditScript(contracts Contracts) []byte {
+	return replaceAddresses(
+		readFile(flowAddInteractionTemplateAuditPath),
+		contracts,
+	)
+}
+
+func flowRevokeInteractionTemplateAuditScript(contracts Contracts) []byte {
+	return replaceAddresses(
+		readFile(flowRevokeInteractionTemplateAuditPath),
+		contracts,
+	)
+}
+
+func flowHasAuditedInteractionTemplateAudit(contracts Contracts) []byte {
+	return replaceAddresses(
+		readFile(flowHasAuditedInteractionTemplateAuditPath),
+		contracts,
+	)
 }
 
 func verifyUserSignaturesScript(contracts Contracts) []byte {
